8da46e0fb8fe1bbe0dba7d5ee7ebed5ac8c5e953
KuKetto <domonkos115@gmail.com>
2023-05-17 22:24:02 +0200

    separated doingSomething method

--- src/Bot/Shield.java
+++ src/Bot/Shield.java
@@ -1,17 +1,64 @@
 package Bot;
 
 import GameData.Planets;
+import Utils.UILogger;
 import WebSocket.WebSocketCommunication;
 import challenge.game.event.action.ErectShieldAction;
 import challenge.game.model.Planet;
 
+import java.util.Calendar;
 import java.util.Random;
 
 public class Shield
 {
+    private long shieldTimer = 0;
+    private long lastShieldAttempt = 0;
     public Shield()
     {
 
+    }
+
+    public void resetShieldTimer(int planet_id) {
+        shieldTimer = 0;
+        lastShieldAttempt = 0;
+        Planets.planetsShielded.removeIf(p -> p.getId() == planet_id);
+    }
+
+    public boolean isShieldReady(boolean isLateGamePhase) {
+        long timeNow = Calendar.getInstance().getTimeInMillis();
+        shieldTimer = shieldTimer - (timeNow - lastShieldAttempt);
+        lastShieldAttempt = timeNow;
+
+        if (isLateGamePhase) {
+            return shieldTimer <= 0 || Planets.planetsShielded.size() < 2;
+        } else {
+            return shieldTimer <= 0 && Planets.planetsShielded.size() < 2;
+        }
+    }
+
+    public ErectShieldAction handleShield(int planet_id) {
+        if (planet_id == -1) {
+            Planet endangeredPlanet = MyDataAnalysis.getDefPlanet();
+            if (endangeredPlanet != null) {
+                return getErectShieldAction(endangeredPlanet);
+            }
+        } else {
+            Planet planet = Planets.getPlanetByID(planet_id);
+            if (planet == null) throw new RuntimeException("Error, target planet does not exists");
+            return getErectShieldAction(planet);
+        }
+        return null;
+    }
+
+    private ErectShieldAction getErectShieldAction(Planet planet) {
+        ErectShieldAction shieldAction = Shield.erectShield(planet);
+        if (shieldAction != null) {
+            Planets.planetsShielded.add(planet);
+            shieldTimer = Controll.game.getSettings().getTimeToBuildShild() + Controll.game.getSettings().getShildDuration();
+            lastShieldAttempt = Calendar.getInstance().getTimeInMillis();
+            return shieldAction;
+        }
+        throw new RuntimeException("An another shield was built in the meantime");
     }
 
     public static ErectShieldAction erectShield(Planet target) {
--- src/Bot/SpaceMission.java
+++ src/Bot/SpaceMission.java
@@ -1,9 +1,11 @@
 package Bot;
 
 import GameData.OnGoingSpaceMissions;
+import GameData.Planets;
 import WebSocket.WebSocketCommunication;
 import challenge.game.event.action.SpaceMissionAction;
 import challenge.game.model.Planet;
+import org.glassfish.grizzly.utils.Pair;
 
 import java.util.Random;
 
@@ -16,6 +18,12 @@ public class SpaceMission
     private void PlanetDensity()
     {
 
+    }
+
+    public SpaceMissionAction planSpaceMission() {
+        Pair<Double, Pair<Planet, Planet>> closestPlanet = Planets.findClosestPlanets(false);
+        if (closestPlanet == null) throw new RuntimeException("There's no planet left to send a space mission...");
+        return Bot.SpaceMission.sendSpaceMission(closestPlanet.getSecond().getFirst(), closestPlanet.getSecond().getSecond());
     }
 
     public static SpaceMissionAction sendSpaceMission(Planet originalPlanet, Planet targetPlanet) {
